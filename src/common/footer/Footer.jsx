19e376b0374d84420063fbafbc664feff22bfbae
1981496647 <1981496647@qq.com>
2022-02-23 17:30:46 +0800

    拉代码

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -3,36 +3,35 @@ import FooterStyle from "./style";
 import logo from "../../assets/img/footer/logo.svg";
 import title from "../../assets/img/footer/title.svg";
 import { list } from "./footerList";
-import { StyledPageContent} from '../../style/common'
+import { StyledPageContent } from "../../style/common";
 const Footer = memo(() => {
   return (
     <FooterStyle>
       <StyledPageContent>
-      <img src={logo} alt="" className="img-logo" />
-      <div className="title">
-        <img src={title} alt="" />
-        <span>Powered by Mammoth Chain</span>
-      </div>
-      <div className="section-footer">
-        MMTScan is a Block Explorer and Analytics Platform for Mammoth Chain
-      </div>
-      <div className="line" />
-      <div className="bottom-footer">
-        <div className="text">
-          MMTScan © 2022 (MMT-A) | ⛏ Built by the team mmtchain.io
+        <img src={logo} alt="" className="img-logo" />
+        <div className="title">
+          <img src={title} alt="" />
+          <span>Powered by Mammoth Chain</span>
         </div>
-        <div className="imglist">
-          {list.map((item) => {
-            return (
-              <div key={item.key} className="img-item">
-                <img src={item.icon} alt=""></img>
-              </div>
-            );
-          })}
+        <div className="section-footer">
+          MMTScan is a Block Explorer and Analytics Platform for Mammoth Chain
         </div>
-      </div>
+        <div className="line" />
+        <div className="bottom-footer">
+          <div className="text">
+            MMTScan © 2022 (MMT-A) | ⛏ Built by the team mmtchain.io
+          </div>
+          <div className="imglist">
+            {list.map((item) => {
+              return (
+                <div key={item.key} className="img-item">
+                  <img src={item.icon} alt=""></img>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </StyledPageContent>
-      
     </FooterStyle>
   );
 });
